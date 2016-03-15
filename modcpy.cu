e8a3d3b60a257028742e1cb19d2f52cdb6e20cea
Evandro Carrijo Taquary <evandro.taquary@gmail.com>
2016-03-15 14:58:51 -0300

    increase performance with vectorized memory access (thanks to Mateus F Freitas)

--- modcpy.cu
+++ modcpy.cu
@@ -2,13 +2,11 @@
 
 __global__ void modcpy(void *destination, void *source, size_t destination_size, size_t source_size){
 
-	int idx = blockIdx.x * blockDim.x + threadIdx.x;
-	int *dp = (int*) destination;
-	int *sp = (int*) source;
+	int idx = blockIdx.x * blockDim.x + threadIdx.x; 
 	int pos;
-
-	for(int i=idx; i<destination_size/sizeof(int); i+=gridDim.x*blockDim.x){
-		pos = i % (source_size/sizeof(int));
-		dp[i] = sp[pos];		
+ 
+	for(int i = idx; i < destination_size/sizeof(int4); i += gridDim.x * blockDim.x){
+		pos = i % (source_size / sizeof(int4));
+		reinterpret_cast<int4*>(destination)[i] = reinterpret_cast<int4*>(source)[pos];  
 	}
 }
