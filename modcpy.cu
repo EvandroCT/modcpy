5abf721fc13160dac94bfcd51759e70510f5a028
Evandro Carrijo Taquary <evandro.taquary@gmail.com>
2016-03-15 15:01:37 -0300

    more performance increase (thanks to Mateus F Freitas)

--- modcpy.cu
+++ modcpy.cu
@@ -5,8 +5,9 @@ __global__ void modcpy(void *destination, void *source, size_t destination_size,
 	int idx = blockIdx.x * blockDim.x + threadIdx.x; 
 	int pos;
  
-	for(int i = idx; i < destination_size/sizeof(int4); i += gridDim.x * blockDim.x){
-		pos = i % (source_size / sizeof(int4));
+	int ds = destination_size/sizeof(int4), ss = source_size/sizeof(int4);
+	for(int i = idx; i < ds; i += gridDim.x * blockDim.x){
+		pos = i % ss;
 		reinterpret_cast<int4*>(destination)[i] = reinterpret_cast<int4*>(source)[pos];  
 	}
 }
